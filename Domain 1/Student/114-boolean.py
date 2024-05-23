918f0d7ac804a1b4778f88203fad3c85ede3995f
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-05-23 08:38:50 -0400

    Update 114-boolean.py

--- Domain 1/Student/114-boolean.py
+++ Domain 1/Student/114-boolean.py
@@ -4,5 +4,4 @@ if char_life == False:
     print ("game over")
 else:
     print ("you are still in the game")    
-
-
+#it fails if I chnage True to lowercase true and try to return this code. We do see a message asking 
