48287a92f2892cbab717ce501dd8f231aa2b96e5
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-05-23 08:54:05 -0400

    Update 122-indexing.py

--- Domain 1/Student/122-indexing.py
+++ Domain 1/Student/122-indexing.py
@@ -1,3 +1,6 @@
 categories = ["Early Bronze Age", "Middle Bronze Age", "Late Bronze Age", "Bronze Age Collapse", "Early Iron Age"]
 print(categories)
 
+printz(categories[0])# A 1 means that we want to print only the first category , the early Bronze age 
+# A 2 means that we want to print now the Late Bronze Age
+# negative numbers will go backward order 
