b35089091146e275098ce149a69d330f579251be
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-05-23 20:14:22 -0400

    Update 126-list_operations.py

--- Domain 1/Student/126-list_operations.py
+++ Domain 1/Student/126-list_operations.py
@@ -1,4 +1,11 @@
 capitals=["Montgomery","Juneau","Phoenix"]
-
-
+capitals.append("sacramento")
+capitals.insert(3,"LittleRock")
+capitals.remove("Juneau")#montgomery,phoenix,LittleRock,Sacramento
+population=[200000,32000,1600000]
+capitals[2]="Little Rock"
+capitals.reverse()
+print(capitals.pop(0))
 print(capitals)
+print(min(population))
+print(len(capitals))
