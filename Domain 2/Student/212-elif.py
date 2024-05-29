f54d3da2dcfa91e6adf23f7bba2dccfa199167e4
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-05-29 08:43:04 -0400

    Update 212-elif.py

--- Domain 2/Student/212-elif.py
+++ Domain 2/Student/212-elif.py
@@ -1,6 +1,6 @@
 score = 8000
 
-if score > 10000:
+if score > 10000: # if statement 
     print("You have reached level 2")
-if score > 5000:
+if score > 5000:  # another if statement 
     print("You have reached level 1")
