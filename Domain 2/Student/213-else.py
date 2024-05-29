66e20c75ddcaf164a4a593d546774dff2099ff6c
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-05-29 08:50:02 -0400

    Update 213-else.py

--- Domain 2/Student/213-else.py
+++ Domain 2/Student/213-else.py
@@ -4,3 +4,8 @@ if score > 10000:
     print("You have reached level 2")
 elif score > 5000:
     print("You have reached level 1")
+else:
+    print("You need to score higher to reach a level")
+
+print("Thank you for playing")
+
